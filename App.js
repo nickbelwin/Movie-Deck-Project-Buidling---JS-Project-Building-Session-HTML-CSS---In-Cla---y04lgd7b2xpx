2855bbc7d25a9d7eae36bb2800ccbd51d33099f2
nickbelwin <belwalkarsarvesh@gmail.com>
2023-10-05 18:47:46 +0530

    favorite filter

--- App.js
+++ App.js
@@ -3,39 +3,8 @@ let sortByDate = false;
 let sortByRate = false;
 let fevorite = false;
 
-const heart = document.querySelectorAll(".heart");
-// console.log(heart);
-let count = 0;
-let favorite = 0;
-heart.forEach((val) => {
-    val.addEventListener("click", (e) => {
-        if (count >= 0) {
-            val.innerHTML = `<i class="ri-heart-fill"></i>`;
-            val.classList.add("love");
-            favorite++;
-            count = count + 1;
-        }
-        else {
-            val.innerHTML = `<i class="ri-heart-line"></i>`;
-            val.classList.remove("love");
-            favorite--;
-            count--;
-        }
-    });
-});
-// console.log(count)
-// console.log(favorite);
 let pageNumber = 1;
 const movieList = document.getElementById("movie-list");
-
-// movieList.addEventListener("click",clickFunc);
-// const allCard=Array.from(document.querySelectorAll(".movie-card"));
-// allCard.forEach((val)=>{
-//     val.addEventListener("click", (e)=>{
-//         console.log(e.target.id);
-//     })
-//     console.log(val);
-// })
 
 async function getMovie() {
     let getData = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${pageNumber}`);
@@ -54,7 +23,52 @@ async function getMovie() {
         document.getElementById("all").classList.add("active-tab");
     }
 }
-
+let arrFevorite=[];
+let fevoriteId=[];
+function liked(e){
+    favorite=true;
+    let loveId=e.target.parentElement.offsetParent.id;
+    fevoriteId.push(e.target.offsetParent.id);
+    document.getElementById(e.target.offsetParent.id).innerHTML=`<i class="ri-heart-fill"></i>`;
+    console.log(loveId);
+    likedMovi(loveId);
+}
+function fevList(){
+    fevoriteId.forEach((id)=>{
+        document.getElementById(id).innerHTML=`<i class="ri-heart-fill"></i>`;
+    })
+}
+async function likedMovi(loveId){
+    let getData = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${pageNumber}`);
+    let data = await getData.json();
+    let userData=data.results;
+    userData.forEach((val)=>{
+        if(val.id == loveId){
+            arrFevorite.push(val);
+        }
+    });
+    console.log(arrFevorite);
+}
+function favoriteMovie(){
+    document.querySelector(".pagination").style.display="none";
+    document.getElementById("like").classList.add("active-tab");
+    document.getElementById("back").style.display="block";
+    document.getElementById("all").style.display="none";
+    document.querySelector(".filter").style.display="none";
+    document.getElementById('headingLine').style.display="flex";
+    movieList.innerHTML="";
+    arrFevorite.forEach((val)=>{
+        movieList.innerHTML += `<li>
+        <div id="${val.id}" class="movie-card" onclick="clickFunc(event)" >
+            <img src="https://image.tmdb.org/t/p/original${val.poster_path}" alt="Images"/>
+            <h3>${val.title}</h3>
+            <span class="heart" id="${val.id+1}" onclick="liked(event)"><i class="ri-heart-fill"></i></span>
+            <h4>Votes: ${val.vote_count}</h4>
+            <h4>Rating: ${val.vote_average}</h4>
+        </div>
+    </li>`
+    });
+}
 async function clickFunc(e){
     e.stopPropagation();
     let getData = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${pageNumber}`);
@@ -83,7 +97,7 @@ async function clickFunc(e){
     })
 // let back=document.getElementById("back");
 // back.addEventListener("click", backPage);
-    console.log(e.target.offsetParent.id);
+    // console.log(e.target.offsetParent.id);
 }
 
 function backPage(){
@@ -92,6 +106,8 @@ function backPage(){
     document.getElementById('like').style.display="flex";
     document.querySelector(".pagination").style.display="flex";
     document.getElementById('back').style.display="none";
+    document.getElementById('headingLine').style.display="none";
+    document.getElementById("like").classList.remove("active-tab");
     getMovie()
 }
 
@@ -123,7 +139,7 @@ function allMovie(movieData) {
             <h4>Votes: ${val.vote_count}</h4>
             <h4>Rating: ${val.vote_average}</h4>
         </div>
-    </li>`
+    </li>`;
     });
 }
 getMovie();
@@ -145,22 +161,6 @@ backbtn.addEventListener("click", () => {
         getMovie();
     }
 });
-
-async function liked(e){
-    e.stopPropagation();
-    let getData = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=f531333d637d0c44abc85b3e74db2186&language=en-US&page=${pageNumber}`);
-    let data = await getData.json();
-    let likeData=data.results;
-    document.querySelector(".heart").innerHTML="";
-    likeData.forEach((val, index)=>{
-        if(val.id+1 == e.target.offsetParent.id){
-            document.querySelector(".heart").classList.add("loved");
-            document.querySelector(".heart").innerHTML=`<i class="ri-heart-fill"></i>`;
-        }
-    });
-    console.log(e.target.offsetParent.id);
-
-}
 
 
 async function searchFromApi() {
@@ -241,7 +241,11 @@ function allPage(){
         dCount=0;
         document.getElementById("byrate").classList.remove("active-tab");
         document.getElementById("bydate").classList.remove("active-tab");
+        document.getElementById("like").classList.remove("active-tab");
+        document.querySelector(".filter").style.display="flex";
+        document.querySelector(".pagination").style.display="flex";
         getMovie();
+        fevList();
 }
 
 
